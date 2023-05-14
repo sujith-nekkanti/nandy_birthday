0af80a386915521a6dedc554b8a1ef88859668f3
sujith-nekkanti <133594760+sujith-nekkanti@users.noreply.github.com>
2023-05-15 00:12:14 +0530

    Update nandy-birthday.py

--- nandy-birthday.py
+++ nandy-birthday.py
@@ -14,7 +14,7 @@ answers = [
 ]
 
 # Create the jigsaw puzzle
-puzzle_image = "https://sujith999awsbucket.s3.us-east-2.amazonaws.com/aws_completion.PNG"
+puzzle_image = "https://sujith999awsbucket.s3.us-east-2.amazonaws.com/IMG20230505161453.jpg"
 puzzle_pieces = []
 for i in range(16):
   puzzle_piece = Image.open(puzzle_image).crop((i * 100, 0, (i + 1) * 100, 100))
